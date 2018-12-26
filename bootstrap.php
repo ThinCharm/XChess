d8113c76261b7af86272a0f147c8768a5f1ad40f
Nicholas <nenglish7721@gmail.com>
2018-12-25 23:23:02 -0500

    Load the env configuration file.

--- bootstrap.php
+++ bootstrap.php
@@ -1,1 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * XChess.
+ * 
+ * The worlds best chess playing website.
+ *
+ * @author XChess Contributors <https://github.com/orgs/XChess/people>
+ *
+ * @link <https://github.com/XChess> XChess Code.
+ */
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load the dotenv configuration file.
+(new Dotenv())->load(__DIR__ . '/xchess.env');
